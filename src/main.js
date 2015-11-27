3ff97c4500de2797170e56ba3fc0556276d4600e
Victor Gil <victor@vicgtorgil.name>
2015-11-27 20:26:46 +0100

    update

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
-var game = new Phaser.Game(800, 600, Phaser.AUTO, '', { preload: preload, create: create, update: update });
-
+var game = new Phaser.Game(800, 450, Phaser.AUTO, '', { preload: preload, create: create, update: update });
+var cursors;
+var player;
 function preload() {
 	game.load.image('balloon', 'assets/balloon.png');
     game.load.image('kaktus', 'assets/cactus.png');    
@@ -12,10 +13,33 @@ function create() {
     //  A simple background for our game
     //game.add.sprite(0, 0, 'kaktus');
 
-    var player;
+    //var player;
 
     player = game.add.sprite(32, game.world.height - 150, 'balloon');
+    game.physics.arcade.enable(player);
+    player.body.bounce.y = 0.1;
+    player.body.gravity.y = -300;
+    player.body.collideWorldBounds = true;
+
+cursors = game.input.keyboard.createCursorKeys();
+
+	console.log(cursors);
 }
 
 function update() {
+
+	//game.physics.arcade.collide(player, platforms);
+
+	//player.body.velocity.y = 10;
+
+    if (cursors.left.isDown)
+    {
+        //  Move to the left
+        player.body.velocity.y = 150;
+        console.log("pulsa");
+        //player.animations.play('left');
+    }
+
+
+
 }
