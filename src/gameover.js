1b76211d47412367fecb40c2e34d87b7182a6827
Adrian Llopis <adrianllopismartinez@gmail.com>
2015-11-28 17:50:25 +0100

    UPDATED: FIXED

--- src/gameover.js
+++ src/gameover.js
@@ -4,7 +4,7 @@ var gameOverState = {
 
 		game.add.sprite(0,0, 'gameover');
 
-		var nameLabel = game.add.text(570,420, score, { font: '40px Arial', fill: '#423927'});
+		var nameLabel = game.add.text(580,425, score, { font: '30px Arial', fill: '#423927'});
 
 		
 		var sKey = game.input.keyboard.addKey(Phaser.Keyboard.S);
